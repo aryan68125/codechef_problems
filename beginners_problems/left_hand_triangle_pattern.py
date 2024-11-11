9f067f93c0e15e2515209452a6f35921394e1cc5
aditya <aditya.kumar@iqinfinite.in>
2024-11-11 10:39:01 +0530

    test commit

---
+++ beginners_problems/left_hand_triangle_pattern.py
@@ -0,0 +1,12 @@
+'''
+Thge code below will print the triangle below codechef problem
+*
+**
+***
+****
+*****
+'''
+for i in range(0,6):
+    for j in range(0,i):
+        print("*", end="")
+    print("")
